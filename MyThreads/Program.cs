5de9da57f0dbbb21bd9408d56d0df44578b5ea76
张宙哲 <zhangzhouzhe@BS-BJB0915>
2022-07-06 14:59:15 +0800

    add

---
+++ MyThreads/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+
+
+
+
+        public void Test()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    Thread.Sleep(10);
+                    Console.WriteLine($"Id:{Thread.CurrentThread.ManagedThreadId},执行");
+                });
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
