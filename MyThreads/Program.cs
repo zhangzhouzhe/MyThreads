5ffe0a58439fc66b535b4f70ff9d01b31cc30d68
zhangzhouzhe <zhangzhouzhe@beisen.com>
2022-07-10 15:27:23 +0800

    add

--- MyThreads/Program.cs
+++ MyThreads/Program.cs
@@ -9,13 +9,28 @@ namespace MyThreads
 {
     class Program
     {
+        private Resource res = new Resource();
         static void Main(string[] args)
         {
-          
+
+
+            Thread.CurrentThread.Name = "Main";
+
+            var p = new Program();
+            var work = new Thread(p.ThreadEntry);
+            work.Name = "Worker";
+            work.Start();
+            p.ThreadEntry();
+            Console.ReadLine();
         }
 
 
-
+        void ThreadEntry()
+        {
+            Monitor.Enter(res);
+            res.Record();
+            Monitor.Exit(res);
+        }
 
         public void Test()
         {
@@ -24,12 +39,22 @@ namespace MyThreads
                 var thread = new Thread(() =>
                 {
                     Thread.Sleep(10);
-                    Console.WriteLine($"Id:{Thread.CurrentThread.ManagedThreadId},执行");
+                    Console.WriteLine($"Id:{Thread.CurrentThread.Name},执行");
                 });
                 thread.Start();
             }
             Console.ReadLine();
         }
 
+    }
+
+    public class Resource
+    {
+        public string Called;
+        public void Record()
+        {
+            Called += $"{Thread.CurrentThread.Name}[{DateTime.Now.Millisecond}]";
+            Console.WriteLine(Called);
+        }
     }
 }
